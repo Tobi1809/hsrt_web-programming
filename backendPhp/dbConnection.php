031c670bc61a08cf43b5b7aafc0b9883ace24cda
Tobias <tobias.muench1789@gmx.de>
2020-06-21 23:14:00 +0200

    dbConnection.php
    - Datenbankverbindung ausgelagert für einfacheren include in files wo es benötigt wird

---
+++ backendPhp/dbConnection.php
@@ -0,0 +1,13 @@
+<?php
+
+//Öffnen der Datenbank-Verbindung
+$dbConnection = mysqli_connect("127.0.0.1", "root", "", "webshop");
+
+		if (!$dbConnection) {
+			echo "Fehler: Konnte nicht mit MySQL verbinden." . PHP_EOL;
+			echo "Debug-Fehlernummer: " . mysqli_connect_errno() . PHP_EOL;
+			echo "Debug-Fehlermledung: " . mysqli_connect_error() . PHP_EOL;
+			exit;
+		}
+        
+?>
