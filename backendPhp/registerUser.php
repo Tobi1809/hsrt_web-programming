fc7d30ce59e7b5deaa4bd432610a66be04e23c54
Dustin_dusTir <du-wal@web.de>
2020-06-21 12:59:12 +0200

    separating email function, to make it more neat
    outsourcing password

--- backendPhp/registerUser.php
+++ backendPhp/registerUser.php
@@ -1,14 +1,6 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
-// Load Composer's autoloader
-require '../vendor/autoload.php';
-
-
+include 'sendEmail.php';
 
 
 //Sind die $_POST Werte vorhanden beim Button-click "Registrieren"
@@ -75,51 +67,4 @@ if (isset($_POST['firstname'])) //#! Check clientside with js if values are set
         exit;
     }
     // here shouldn't be any code! #?
-}
-
-function sendRegistrationEmail($firstname, $lastname, $empfaenger_email)
-{
-
-    $betreff = "Registrierung bei Shopp33";
-    $nachricht = "<p>Hallo, Vielen Dank für die Registrierung bei <strong>Shop33</strong> <br> Viel Spaß beim shoppen! </p>";
-
-    // $mailsuccess = mail($empfaenger_email, $betreff, $nachricht, "From: Absender <shopp33prozent@gmail.com>");
-
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
-
-    try {
-        //Server settings
-        $mail->SMTPDebug = 1;                      // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'shop33prozent@gmail.com';                     // SMTP username
-        $mail->Password   = 'nö';                               // SMTP password
-        $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-        //Recipients
-        $mail->setFrom('shop33prozent@gmail.com', 'Mailer');
-        $mail->addAddress($empfaenger_email);     // Add a recipient
-        //$mail->addAddress('ellen@example.com');               // Name is optional
-        //$mail->addReplyTo('info@example.com', 'Information');
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
-
-        // Attachments
-        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-        // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = $betreff;
-        $mail->Body    = $nachricht;
-        $mail->AltBody = strip_tags($nachricht);
-
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
 }
