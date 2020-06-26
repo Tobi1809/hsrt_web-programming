bbf8a9568ffe5fb159e462d63f91e690b750982e
Tobias <tobias.muench1789@gmx.de>
2020-06-26 20:07:01 +0200

    BackendPhp für Javascript in checkout.php
    - Anzeigen der Versandkosten und Gesamtbetrag für den User beim Einkaufen

---
+++ backendPhp/getShippingCostsForUser.php
@@ -0,0 +1,27 @@
+<?php
+
+    if ($_GET["selection"] == "5")
+	{
+		$shippingCosts = 5;
+
+		echo insertShippingCosts($shippingCosts);
+	}
+	elseif ($_GET["selection"] == "15") 
+	{
+		$shippingCosts = 15;
+
+		echo insertShippingCosts($shippingCosts);
+    }
+    elseif ($_GET["selection"] == "0")
+    {
+        $shippingCosts = 0;
+
+        echo insertShippingCosts($shippingCosts);
+    }
+
+    function insertShippingCosts($shippingCosts)
+    {
+        return $shippingCosts;
+    }
+    
+?>
---
+++ backendPhp/getTotalAmountForUsers.php
@@ -0,0 +1,51 @@
+<?php
+
+    //Session starten
+    session_start();
+
+    //Öffnen der Datenbank-Verbindung
+    include ("../backendPhp/dbConnection.php");
+
+    //Die Klasse verfügbar machen
+    include_once ("../backendPhp/cart.php");
+
+    //Eine Neue Instanz der Klasse cart erstellen
+    $cart = new Cart();
+
+
+    $subTotal = 0;
+    $Array = $_SESSION['cartArray'];
+    
+    for($i = 0 ; $i < count($Array); $i++)
+    {
+        $innerArray = $Array[$i];
+        $subTotal+= $innerArray[4];
+    }
+
+    if ($_GET["selection"] == "5")
+	{
+        $shippingCosts = 5;
+        $totalAmount = $subTotal + $shippingCosts;
+
+		echo insertTotalAmount("$totalAmount");
+	}
+	elseif ($_GET["selection"] == "15") 
+	{
+		$shippingCosts = 15;
+        $totalAmount = $subTotal + $shippingCosts;
+
+		echo insertTotalAmount("$totalAmount");
+    }
+    elseif ($_GET["selection"] == "0")
+    {
+        $shippingCosts = 0;
+
+        echo "$subTotal €";
+    }
+
+    function insertTotalAmount($totalAmount)
+    {
+        return "$totalAmount";
+    }
+    
+?>
