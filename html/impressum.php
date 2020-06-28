45b6edd1063c476e88dcd03a20ea69d41de612aa
Tobias <tobias.muench1789@gmx.de>
2020-06-28 21:35:47 +0200

    Impressum fertig

---
+++ html/impressum.php
@@ -0,0 +1,224 @@
+<?php
+
+//Session starten
+session_start();
+
+//Die Klasse verfügbar machen
+include_once("../backendPhp/cart.php");
+
+//Eine Neue Instanz der Klasse cart erstellen
+$cart = new Cart();
+
+//Falls Produkte in der Session bereits im Warenkorb - dann zeige diese an
+$productCount = $cart->get_cart_count();
+
+
+$welcomeString = "";
+//create welcome string if logged in 
+if (isset($_SESSION["login"])) {
+    if ($_SESSION["login"] == 111) {
+        //we are logged in
+        $welcomeString .= "Hallo, ";
+        $welcomeString .=  $_SESSION["firstname"];
+        $welcomeString .= " ";
+        $welcomeString .=  $_SESSION["lastname"];
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+
+    <title>Webshop - Impressum</title>
+    <meta charset="utf-8">
+
+    <!-- Our Styles -->
+    <link rel="stylesheet" href="../css/headerArea.css">
+    <link rel="stylesheet" href="../css/impressum.css">
+
+    <!-- Webseite responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Stylesheet & FontAwesome für Icons -->
+    <link rel="stylesheet" href="../css/w3.css">
+    <link rel="stylesheet" href="../libraries/FontAwesome/CSS/all.css">
+
+    <!-- Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+    <!-- User online Anzeige -->
+    <script>
+        $(document).ready(function() { // wichtig!
+
+            setInterval(function() {
+                $.get("../backendPhp/getNumActiveUsers.php", {},
+                    function(numActiveUsers) {
+                        var activeUserElement = document.getElementById("numUserOnline");
+                        activeUserElement.innerText = numActiveUsers;
+                        console.log("updated active users");
+                    });
+            }, 1000);
+
+        });
+    </script>
+
+</head>
+
+<body class="impressum">
+
+    <!-- Kopfbereich -->
+    <header class="titleBand w3-padding-16">
+        <div class="w3-bar w3-center">
+            <h1 class="myTitle">shop<strong class="myTitle">33</strong></h1>
+            <p class="myTitle">Only the greatest discounts!</p>
+        </div>
+
+        <div class="centerMargin"><a href="home.php" class="w3-button"></i> Home</a></div>
+
+        <div class="centerMargin"><a href="#überuns.html" class="w3-button"> Über uns</a></div>
+
+        <div></div>
+
+        <div class="centerMargin">
+            <h3 class="myTitle"><?php echo $welcomeString ?></h3>
+        </div>
+
+        <div></div>
+
+
+
+        <div class="centerMargin">
+            <!-- shopping cart -->
+            <a href="shoppingCart.php"><i class="fa fa-shopping-cart fa-4x"></i>(<?php echo $productCount ?>)</a>
+        </div>
+
+        <div class="centerMargin">
+            <?php
+            //show My Orders Button if logged in
+            $MyOrdersHtml = '<a href="myOrders.php" class=" w3-button w3-light-gray"><i class="fas fa-box-open"></i>My Orders</a>';
+            if (isset($_SESSION["login"])) {
+                if ($_SESSION["login"] == 111) {
+                    echo $MyOrdersHtml;
+                }
+            }
+
+            ?>
+        </div>
+
+        <div class="centerMargin">
+            <?php
+            // Login, wenn User noch nicht angemeldet ist und Logout, wenn er angemeldet ist
+            $loginHTML = '<a href="login.php" class=" w3-button w3-light-gray"><i class="fas fa-user"></i> Login</a>';
+            $logoutHTML = '<a href="logout.php" class=" w3-button w3-light-gray"><i class="fas fa-user"></i> Logout</a>';
+            if (isset($_SESSION["login"])) {
+                if ($_SESSION["login"] == 111) {
+                    echo $logoutHTML;
+                } else {
+                    echo $loginHTML;
+                }
+            } else {
+                echo $loginHTML;
+            }
+            ?>
+        </div>
+    </header>
+
+    <!-- Impressum -->
+    <article>
+        <div class="impressum-entry clearfix">
+            <h1 class="impressum">Impressum</h1>
+            <h2 class="impressum">Angaben gemäß § 5 TMG</h2>
+            <p class="impressum">Dustin Niklas Walker &amp; Tobias Münch
+                <br /> Hochschule Reutlingen
+                <br /> Alteburgstr. 150
+                <br /> 72762 Reutlingen
+            </p>
+            <h2 class="impressum">Kontakt</h2>
+            <p class="impressum">Telefon: 0170-123456789
+                <br /> E-Mail: shop33prozent@gmail.com
+            </p>
+            <h2 class="impressum">Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h2>
+            <p class="impressum">Dustin Niklas Walker &amp; Tobias Münch
+                <br /> Hochschule Reutlingen
+                <br /> Alteburgstr. 150
+                <br /> 72762 Reutlingen
+            </p>
+            <h3 class="impressum">Haftung für Inhalte</h3>
+            <p class="impressum">Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten
+                nach den
+                allgemeinen
+                Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+                übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die
+                auf
+                eine rechtswidrige Tätigkeit hinweisen.
+            </p>
+            <p class="impressum">Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den
+                allgemeinen Gesetzen
+                bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis
+                einer
+                konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir
+                diese
+                Inhalte umgehend entfernen.
+            </p>
+            <h3 class="impressum">Haftung für Links</h3>
+            <p class="impressum">Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen
+                Einfluss haben.
+                Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+                verlinkten
+                Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten
+                wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren
+                zum
+                Zeitpunkt der Verlinkung nicht erkennbar.
+            </p>
+            <p class="impressum">Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete
+                Anhaltspunkte einer
+                Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+                umgehend entfernen.
+            </p>
+            <h3 class="impressum">Urheberrecht</h3>
+            <p class="impressum">Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten
+                unterliegen dem deutschen
+                Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+                Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+                Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+            </p>
+            <p class="impressum">Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die
+                Urheberrechte Dritter
+                beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+                Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden
+                von
+                Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+            </p>
+            <p class="impressum">Quelle: <a href="https://www.e-recht24.de">eRecht24</a></p>
+        </div>
+    </article>
+
+    <!-- Fußleiste -->
+    <footer class="w3-container w3-padding-16 w3-margin-top">
+        <div class="w3-bar w3-light-gray">
+            <span class="myBarItem w3-light-gray w3-left"> User online: <ins id="numUserOnline">0</ins></span>
+
+            <a href="impressum.php" class="myBarItem w3-button w3-right"> Impressum</a>
+            <a href="#kontakt.html" class="myBarItem w3-button w3-right"><i class="fas fa-envelope"></i>
+                Kontakt</a>
+            <!-- <span class="myBarItem w3-light-gray w3-right">Sie waren zuletzt online am <ins></ins></span> -->
+            <?php
+            if (isset($_SESSION["login"])) {
+                if ($_SESSION["login"] == 111) {
+                    $dateString = date("d.m.Y", $_SESSION['lastLoginTime']);
+                    echo '<span class="myBarItem w3-light-gray w3-right">Sie waren zuletzt online am: <ins>' . $dateString . '</ins></span>';
+                }
+            }
+            ?>
+        </div>
+    </footer>
+
+</body>
+
+</html>
