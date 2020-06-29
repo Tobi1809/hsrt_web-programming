709e03671d5c717632ad7e7116506cedf3f1c6ed
Tobias <tobias.muench1789@gmx.de>
2020-06-29 21:10:45 +0200

    Kontaktformular
    - frontend und backend für das versenden der Anfrage an unsere Emailadresse

--- backendPhp/sendEmail.php
+++ backendPhp/sendEmail.php
@@ -126,3 +126,56 @@ function sendOrderConfirmationEmail($firstname, $lastname, $empfaenger_email, $b
         //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
+
+function receiveContactForm($firstname, $lastname, $entered_email, $empfaenger_email, $topic, $message)
+{
+    $betreff = "Kontaktformular-Anfrage zum Thema: $topic";
+    $nachricht = "<h3>Der User: $firstname $lastname, mit der Email: $entered_email hat per Kontaktformular eine Anfrage gesendet.</h3><br>";
+    $nachricht .= "<p>Inhalt der Anfrage: $message <br>";
+    $nachricht .= "Bitte die Anfrage so schnell wie möglich bearbeiten und ihm eine Antwort per Email senden. <br>";
+    $nachricht .= "Vielen Dank!</p>"
+
+    try {
+        //get Password
+        include '../../info.php';
+        $password = $word;    //password should be saved aoutside of the scope of the public git repository
+
+        //Server settings
+        //$mail->SMTPDebug = 1;                                     // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'shop33prozent@gmail.com';              // SMTP username
+        $mail->Password   = $password;                              // SMTP password
+        $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom('shop33prozent@gmail.com', 'Shopp33');
+        $mail->addAddress($empfaenger_email);     // Add a recipient
+        //$mail->addAddress('ellen@example.com');               // Name is optional
+        //$mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+
+        // Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = $betreff;
+        $mail->Body    = $nachricht;
+        $mail->AltBody = strip_tags($nachricht);
+
+        $mail->send();
+        //echo 'Message has been sent';
+    } catch (Exception $e) {
+        //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
+?>
+
+
+
