e594beab8a83c62a736966b59d727f732c4455cd
Tobias <tobias.muench1789@gmx.de>
2020-06-29 14:27:32 +0200

    shoppingcart.php - Header + Footer aktualisiert
    Zusätzlich auto formatting

--- html/shoppingCart.php
+++ html/shoppingCart.php
@@ -50,15 +50,15 @@ if (isset($_SESSION["login"])) {
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <!-- our Styles -->
-    <link rel="stylesheet" href="../css/headerArea.css">
+    <link rel="stylesheet" href="../css/headerAndFooterArea.css">
     <link rel="stylesheet" href="../css/productsGrid.css">
 
     <script>
-        $(document).ready(function() { // wichtig!
+        $(document).ready(function () { // wichtig!
 
-            setInterval(function() {
+            setInterval(function () {
                 $.get("../backendPhp/getNumActiveUsers.php", {},
-                    function(numActiveUsers) {
+                    function (numActiveUsers) {
                         var activeUserElement = document.getElementById("numUserOnline");
                         activeUserElement.innerText = numActiveUsers;
                         console.log("updated active users");
@@ -73,43 +73,61 @@ if (isset($_SESSION["login"])) {
 <body>
 
     <!-- Kopfbereich -->
-    <header class="titleBand w3-padding-16">
+    <header class="titleBand w3-padding-8">
 
-        <div class="3-bar w3-center">
+        <div class="w3-bar w3-center">
             <h1 class="myTitle">shop<strong class="myTitle">33</strong></h1>
             <p class="myTitle">Only the greatest discounts!</p>
         </div>
 
-        <div class="centerMargin"><a href="home.php" class=" w3-button "></i> Home</a></div>
+        <div class="centerMargin"><a href="home.php">Home</a></div>
 
-        <div class="centerMargin"><a href="aboutUs.php" class=" w3-button"> Über uns</a></div>
+        <div class="centerMargin"><a href="aboutUs.php"> Über uns</a></div>
 
         <div></div>
 
         <div class="centerMargin">
-            <h3 class="myTitle"><?php echo $welcomeString ?></h3>
+            <div>
+                <h3 class="myTitle"><?php echo $welcomeString ?></h3>
+            </div>
         </div>
 
         <div></div>
+
         <div class="centerMargin">
             <!-- shopping cart -->
-            <a href="#"> <i class="fa fa-shopping-cart fa-4x"></i></a>
+            <a href="shoppingCart.php"><i class="fa fa-shopping-cart fa-2x"></i>(<?php echo $productCount ?>)</a>
         </div>
 
-        <?php
-        // Login, wenn User noch nicht angemeldet ist und Logout, wenn er angemeldet ist
-        $loginHTML = '<div class="centerMargin"><a href="login.php" class=" w3-button w3-light-gray"><i class="fas fa-user"></i> Login</a></div>';
-        $logoutHTML = '<div class="centerMargin"><a href="logout.php" class=" w3-button w3-light-gray"><i class="fas fa-user"></i> Logout</a></div>';
-        if (isset($_SESSION["login"])) {
-            if ($_SESSION["login"] == 111) {
-                echo $logoutHTML;
+        <div class="centerMargin">
+            <?php
+            //show My Orders Button if logged in
+            $MyOrdersHtml = '<a href="myOrders.php" class="centerMargin"><i class="fas fa-box-open"></i> Meine Bestellungen</a>';
+            if (isset($_SESSION["login"])) {
+                if ($_SESSION["login"] == 111) {
+                    echo $MyOrdersHtml;
+                }
+            }
+
+            ?>
+        </div>
+
+        <div class="centerMargin">
+            <?php
+            // Login, wenn User noch nicht angemeldet ist und Logout, wenn er angemeldet ist
+            $loginHTML = '<a href="login.php" class="centerMargin"><i class="fas fa-user"></i> Login</a>';
+            $logoutHTML = '<a href="logout.php" class="centerMargin"><i class="fas fa-user"></i> Logout</a>';
+            if (isset($_SESSION["login"])) {
+                if ($_SESSION["login"] == 111) {
+                    echo $logoutHTML;
+                } else {
+                    echo $loginHTML;
+                }
             } else {
                 echo $loginHTML;
             }
-        } else {
-            echo $loginHTML;
-        }
-        ?>
+            ?>
+        </div>
 
     </header>
 
@@ -183,7 +201,7 @@ if (isset($_SESSION["login"])) {
 
             if (isset($_POST['emptyCart'])) {
                 $cart->reset_cart();
-                echo "<script>location.href=location.href;</script>";
+                echo "<script>location.href = location.href;</script>";
             }
 
             ?>
@@ -196,15 +214,17 @@ if (isset($_SESSION["login"])) {
 
                 if ($productCount == 0) {
             ?><span class="text-danger align-middle" id="invalidCredentialsErrorMessage">
-                        <!-- Error message here -->
-                        <i class="fa fa-close animate__animated animate__shakeX"></i>Füllen Sie bitte zuerst Ihren Warenkorb um fortzufahren!
-                    </span>
+                <!-- Error message here -->
+                <i class="fa fa-close animate__animated animate__shakeX"></i> Füllen Sie bitte zuerst Ihren Warenkorb um
+                fortzufahren!
+            </span>
             <?php
                 }
                 if (isset($_SESSION["login"]) && $productCount > 0) {
                     if ($_SESSION["login"] == 111) {
                         $userLogin = true;
                         $uid = $_SESSION['uid'];
+                        $Array = $_SESSION['cartArray'];
 
                         header("Location: checkout.php");
                     }
@@ -219,13 +239,32 @@ if (isset($_SESSION["login"])) {
     </form>
 
     <!-- Fußleiste -->
-    <footer class="w3-container w3-padding-16 w3-margin-top">
-        <div class="w3-bar w3-light-gray">
-            <span class="myBarItem w3-light-gray w3-left"> User online: <ins id="numUserOnline">0</ins></span>
-            <a href="contactForm.php" class="myBarItem w3-button w3-right"><i class="fas fa-envelope"></i>
-                Kontakt</a>
-            <a href="impressum.php" class="myBarItem w3-button w3-right"> Impressum</a>
+    <footer class="footer w3-padding-32">
+
+        <div class="centerMargin"><a href="impressum.php">Impressum</a></div>
+        <div class="centerMargin"><a href="contactForm.php"><i class="fas fa-envelope"></i> Kontakt</a></div>
+
+        <div></div>
+        <div></div>
+        <div></div>
+
+        <div class="centerMargin">
+            <?php
+            if (isset($_SESSION["login"])) {
+                if ($_SESSION["login"] == 111) {
+                    $dateString = date("d.m.Y", $_SESSION['lastLoginTime']);
+                    echo '<span>Sie waren zuletzt am <ins>' . $dateString . '</ins> online</span>';
+                }
+            }
+        ?>
         </div>
+
+        <div></div>
+
+        <div class="centerMargin">
+            <span><ins id="numUserOnline"></ins> User online</span>
+        </div>
+
     </footer>
 
 </body>
