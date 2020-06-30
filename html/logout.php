90e29983243f311d87465d2c018c06944124ea7d
Tobias <tobias.muench1789@gmx.de>
2020-06-30 18:54:16 +0200

    Zusatz für logout.php
    - Hat er damals auch drin gehabt im logout

--- html/logout.php
+++ html/logout.php
@@ -8,7 +8,6 @@ if ($_SESSION['login'] != 111)
     header("Location: login.php");
 }
 
-
 try {
     //Öffnen der Datenbank-Verbindung
     $dbConnection = mysqli_connect("127.0.0.1", "root", "", "webshop");
@@ -32,7 +31,20 @@ try {
     echo "Error Connecting to database";
 }
 
+// Löschen aller Session-Variablen.
+$_SESSION = array();
 
+/* Falls die Session gelöscht werden soll, löschen Sie auch das Session-Cookie.
+// Achtung: Damit wird die Session gelöscht, nicht nur die Session-Daten!
+*/
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params["path"],
+        $params["domain"], $params["secure"], $params["httponly"]
+    );
+}
+
+// Zum Schluss - löschen der Session
 session_destroy();
 
 header("Location: home.php");
