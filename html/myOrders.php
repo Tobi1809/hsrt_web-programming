b6f592e2efe8cfb4be6ee1b85b2227f6131c245b
Dustin_dusTir <du-wal@web.de>
2020-07-01 20:55:50 +0200

    small bugfix

--- html/myOrders.php
+++ html/myOrders.php
@@ -239,7 +239,7 @@ if (isset($_SESSION["login"])) {
                                 <form method="post" action="../backendPhp/processOrder.php" name="payment" class="form-horizontal">
 
                                     <input type="text" name="firstname" id="firstname" value="<?php echo $_SESSION["firstname"]; ?>" hidden>
-                                    <input type="text" name="lastname" id="lastname" value="<?php echo $_SESSION["lastname-"]; ?>" hidden>
+                                    <input type="text" name="lastname" id="lastname" value="<?php echo $_SESSION["lastname"]; ?>" hidden>
                                     <input type="text" name="city" id="city" value="<?php echo $shippingAdress; ?>" hidden>
                                     <input type="text" name="zip" id="zip" value="<?php echo $shippingAdress; ?>" hidden>
                                     <input type="text" name="street" id="street" value="<?php echo $shippingAdress; ?>" hidden>
